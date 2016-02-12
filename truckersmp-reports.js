4075632cfdff59272d5e3e2756e0ee4da70a2a71
CJMAXiK <i@cjmaxik.ru>
2016-02-12 23:44:08 +1000

    Aliases issues

--- truckersmp-reports.js
+++ truckersmp-reports.js
@@ -2,7 +2,7 @@
 // @name         TruckersMP Reports Improved
 // @description  Only for TruckersMP Admins
 // @namespace    http://truckersmp.com/
-// @version      1.2
+// @version      1.2.1
 // @author       CJMAXiK
 // @match        *://truckersmp.com/*/reports/view/*
 // @homepageURL  https://openuserjs.org/scripts/cjmaxik/TruckersMP_Reports_Improved
@@ -20,7 +20,7 @@
 // ==/OpenUserJS==
 /* jshint -W097 */
 'use strict';
-var $version = "1.2";
+var $version = "1.2.1";
 console.log("TruckersMP Reports Improved INBOUND! Question - to @cjmaxik on Slack!");
 $('body > div.wrapper > div.breadcrumbs > div > h1').append(' Improved <span class="badge" data-toggle="tooltip" title="by @cjmaxik">' + $version + '</span> <a href="#" data-toggle="modal" data-target="#script-settings"><i class="fa fa-cog" data-toggle="tooltip" title="Script settings"></i></a> <i class="fa fa-spinner fa-spin" id="loading-spinner"></i>');
 
@@ -178,7 +178,7 @@ if (steamapi === "Kappa") {
                 xhr: function(){return new GM_XHR();},
                 type: 'GET',
                 success: function(val){
-                    var steam_aliases = GM_getValue("aliases_data");
+                    var steam_aliases = val;
                     var aliases = "";
                     for(var key in steam_aliases) {
                         aliases += '<kbd  data-toggle="tooltip" title="' + steam_aliases[key].timechanged + '">' + steam_aliases[key].newname + '</kbd>   ';
