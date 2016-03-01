300efcdd73473007e9dac5be434da1cb33050edb
CJMAXiK <i@cjmaxik.ru>
2016-03-01 17:52:31 +1000

    1.4.1 deployed!

--- truckersmp-reports.js
+++ truckersmp-reports.js
@@ -2,7 +2,7 @@
 // @name         TruckersMP Reports Improved
 // @description  Only for TruckersMP Admins
 // @namespace    http://truckersmp.com/
-// @version      1.4.0
+// @version      1.4.1
 // @author       CJMAXiK
 // @match        *://truckersmp.com/*/reports/view/*
 // @homepageURL  https://openuserjs.org/scripts/cjmaxik/TruckersMP_Reports_Improved
@@ -17,7 +17,7 @@
 // ==/OpenUserJS==
 /* jshint -W097 */
 'use strict';
-var version = "1.4.0";
+var version = "1.4.1";
 console.log("TruckersMP Reports Improved INBOUND! Question - to @cjmaxik on Slack!");
 $('body > div.wrapper > div.breadcrumbs > div > h1').append(' Improved <span class="badge" data-toggle="tooltip" title="by @cjmaxik">' + version + '</span> <a href="#" data-toggle="modal" data-target="#script-settings"><i class="fa fa-cog" data-toggle="tooltip" title="Script settings"></i></a> <a href="http://bit.ly/BlameAnybody" target="_blank" id="version_detected" data-toggle="popover" data-trigger="focus" title="YAY! v.' + version + ' has been deployed!" data-content="Your handy-dandy script just updated! See what you get?"><i class="fa fa-question" data-toggle="tooltip" title="Changelog"></i></a> <i class="fa fa-spinner fa-spin" id="loading-spinner"></i>');
 
@@ -48,7 +48,7 @@ $('.content').each(function(){
 var steam_id = $('input[name="steam_id"]').val();
 var storage = $.localStorage;
 var steamapi = storage.get('SteamApi');
-var last_version = storage.get('last_version');
+var last_version = storage.get('truckersmp-reports-last_version');
 var OwnReasons = storage.get('OwnReasons');
 
 // ===== Versioning =====
