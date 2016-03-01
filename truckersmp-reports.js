c8b6e7f4027e99fdc33abc0fdc3cbe73a0b2e0eb
CJMAXiK <i@cjmaxik.ru>
2016-03-01 15:49:01 +1000

    1.4.0 deployed!
    - Own Reasons buttons!!!! YAAAAAAAAAAAY!
    - Some fixes

--- truckersmp-reports.js
+++ truckersmp-reports.js
@@ -2,7 +2,7 @@
 // @name         TruckersMP Reports Improved
 // @description  Only for TruckersMP Admins
 // @namespace    http://truckersmp.com/
-// @version      1.3.2
+// @version      1.4.0
 // @author       CJMAXiK
 // @match        *://truckersmp.com/*/reports/view/*
 // @homepageURL  https://openuserjs.org/scripts/cjmaxik/TruckersMP_Reports_Improved
@@ -17,9 +17,10 @@
 // ==/OpenUserJS==
 /* jshint -W097 */
 'use strict';
-var version = "1.3.2";
+var version = "1.4.0";
 console.log("TruckersMP Reports Improved INBOUND! Question - to @cjmaxik on Slack!");
 $('body > div.wrapper > div.breadcrumbs > div > h1').append(' Improved <span class="badge" data-toggle="tooltip" title="by @cjmaxik">' + version + '</span> <a href="#" data-toggle="modal" data-target="#script-settings"><i class="fa fa-cog" data-toggle="tooltip" title="Script settings"></i></a> <a href="http://bit.ly/BlameAnybody" target="_blank" id="version_detected" data-toggle="popover" data-trigger="focus" title="YAY! v.' + version + ' has been deployed!" data-content="Your handy-dandy script just updated! See what you get?"><i class="fa fa-question" data-toggle="tooltip" title="Changelog"></i></a> <i class="fa fa-spinner fa-spin" id="loading-spinner"></i>');
+
 
 // ===== Bootstrapping =====
 var now = moment();
@@ -32,32 +33,8 @@ var date_buttons = '<br>' +
     '<button type="button" class="btn btn-danger plusdate" data-plus="3month">+3</button>' +
     '<button type="button" class="btn btn-xs btn-link plusdate" data-plus="clear">Current time</button>';
 
-var reason_buttons = '<br><div class="btn-group"><a class="btn btn-warning dropdown-toggle" data-toggle="dropdown" href="#">Prefixes <span class="caret"></span></a>' +
-    '<ul class="dropdown-menu">' +
-    '    <li><a href="#" class="plusreason" data-place="before">Intentional</a></li>' +
-    '</ul></div>' +
-    '     <div class="btn-group"><a class="btn btn-default dropdown-toggle" data-toggle="dropdown" href="#">Reasons <span class="caret"></span></a>' +
-    '<ul class="dropdown-menu">' +
-    '    <li><a href="#" class="plusreason" data-place="after">Ramming</a></li>' +
-    '    <li><a href="#" class="plusreason" data-place="after">Blocking</a></li>' +
-    '    <li><a href="#" class="plusreason" data-place="after">Wrong Way</a></li>' +
-    '    <li><a href="#" class="plusreason" data-place="after">Insulting</a></li>' +
-    '    <li><a href="#" class="plusreason" data-place="after">Trolling</a></li>' +
-    '    <li><a href="#" class="plusreason" data-place="after">Reckless driving</a></li>' +
-    '    <li><a href="#" class="plusreason" data-place="after">Offensive language</a></li>' +
-    '</ul></div>' +
-    '     <div class="btn-group"><a class="btn btn-danger dropdown-toggle" data-toggle="dropdown" href="#">Postfixes <span class="caret"></span></a>' +
-    '<ul class="dropdown-menu">' +
-    '    <li><a href="#" class="plusreason" data-place="after">// 1 m due to history</a></li>' +
-    '    <li><a href="#" class="plusreason" data-place="after">// 3 m due to history</a></li>' +
-    '    <li><a href="#" class="plusreason" data-place="after">// Perma due to history</a></li>' +
-    '</ul></div>' +
-    '    <button type="button" class="btn btn-link" id="reason_clear">Clear</button>';
-
 $(date_buttons).insertAfter('#confirm-accept > div > div > form > div.modal-body > div:nth-child(5) > label:nth-child(4)');
 $('input[id="perma.false"]').prop("checked", true);
-
-$(reason_buttons).insertAfter('#confirm-accept > div > div > form > div.modal-body > div:nth-child(6) > input');
 
 // ===== Links in content =====
 $('.content').each(function(){
@@ -72,6 +49,7 @@ var steam_id = $('input[name="steam_id"]').val();
 var storage = $.localStorage;
 var steamapi = storage.get('SteamApi');
 var last_version = storage.get('last_version');
+var OwnReasons = storage.get('OwnReasons');
 
 // ===== Versioning =====
 if (version != last_version) {
@@ -79,7 +57,21 @@ if (version != last_version) {
     $('#version_detected').popover('show');
 } else {
     storage.set('last_version', version);
+}
+
+// ==== OwnReasons buttons
+var default_OwnReasons = {
+    prefixes: "Intentional",
+    reasons: "Ramming, Blocking, Wrong Way, Insulting, Trolling, Reckless Driving, Offensive language",
+    postfixes: "// 1 m due to history, // 3 m due to history, // Perma due to history"
 };
+if (!storage.isSet('OwnReasons') || storage.isEmpty('OwnReasons')) {
+    storage.set('OwnReasons', default_OwnReasons);
+    OwnReasons = default_OwnReasons;
+}
+
+var reason_buttons = construct_buttons(OwnReasons);
+$(reason_buttons).insertAfter('#confirm-accept > div > div > form > div.modal-body > div:nth-child(6) > input');
 
 // ==== Settings Modal =====
 var settings_modal = '<div class="modal fade ets2mp-modal" id="script-settings" tabindex="-1">' +
@@ -94,72 +86,50 @@ var settings_modal = '<div class="modal fade ets2mp-modal" id="script-settings" 
                     'If you don\'t want to use Steam integration, click on Kappa <img src="http://www.rivsoft.net/content/icons/kappa_big.png" id="Kappa">' +
                 '</div>'+
                 '<hr>'+
-                '<h4>Own PlusDate Buttons (piece of shiet, just forget about it)</h4>' +
-                '<hr>'+
-                '<h3>Own Reasons Buttons (stay tuned)</h3>' +
+                '<h3>Own Reasons Buttons <small>(use Comma symbol to split variants)</small></h3>' +
                 '<div class="form-group">'+
-                    '<label for="plusdate-own-buttons">Own code</label> <textarea class="form-control" rows="3" id="plusdate-own-buttons" name="plusdate-own-buttons" disabled>[{"type": "Reason", "text": "Insulting"},  {"type": "Prefix", "text": "Intensional"}, {"type": "Postfix", "text": "// FACK YOU"}]</textarea>' +
+                    '<label for="plusreason-own-Prefixes">Prefixes</label> <textarea class="form-control" rows="3" id="plusreason-own-Prefixes" name="plusreason-own-Prefixes"></textarea>' +
                 '</div>'+
-                '<div class="form-group clearfix">'+
-                    '<div class="col-md-6 col-xs-12">' +
-                    '   <h3>Syntax:</h3>' +
-                    '   <p><kbd>[{"type": "Reason", "text": "LOLOLOL"}, ...]</kbd></p>' +
-                    '   <table class="table table-striped table-bordered">' +
-                    '     <tbody>'+
-                    '       <tr>'+
-                    '         <th>Key</th>'+
-                    '         <th>Explanation</th>'+
-                    '       </tr>'+
-                    '       <tr>'+
-                    '         <td><kbd>type</kbd></td>'+
-                    '         <td>Type of text (see table ->). <b>Required</b>.</td>'+
-                    '       </tr>'+
-                    '       <tr>'+
-                    '         <td><kbd>text</kbd></td>'+
-                    '         <td>Text LOL. <b>Required</b>.</td>'+
-                    '       </tr>'+
-                    '     </tbody>'+
-                    '   </table>'+
-                    '</div>' +
-                    '<div class="col-md-6 col-xs-12">' +
-                    '   <h3>"bit" variations: </h3>' +
-                    '   <table class="table table-striped table-bordered">' +
-                    '     <tbody>'+
-                    '       <tr>'+
-                    '         <th>Key</th>'+
-                    '         <th>Explanation</th>'+
-                    '       </tr>'+
-                    '       <tr>'+
-                    '         <td>Reason</td>'+
-                    '         <td>Main text, placed after all the text + Space symbol</td>'+
-                    '       </tr>'+
-                    '       <tr>'+
-                    '         <td>Prefix</td>'+
-                    '         <td>Placed before all the text + Space symbol</td>'+
-                    '       </tr>'+
-                    '       <tr>'+
-                    '         <td>Postfix</td>'+
-                    '         <td>Like Reasons, but it is not main.</td>'+
-                    '       </tr>'+
-                    '     </tbody>'+
-                    '   </table>'+
-                    '</div>'+
+                '<div class="form-group">'+
+                   '<label for="plusreason-own-Reasons">Reasons</label> <textarea class="form-control" rows="3" id="plusreason-own-Reasons" name="plusreason-own-Reasons"></textarea>' +
+                '</div>'+
+                '<div class="form-group">'+
+                   '<label for="plusreason-own-Postfixes">Postfixes</label> <textarea class="form-control" rows="3" id="plusreason-own-Postfixes" name="plusreason-own-Postfixes"></textarea>' +
                 '</div>'+
             '</div>'+
             '<div class="modal-footer">'+
-                '<button class="btn btn-default" data-dismiss="modal" type="button">Cancel</button> <button class="btn btn-danger" id="script-settings-submit">Save</button>'+
+                '<button class="btn btn-default" data-dismiss="modal" type="button">Cancel</button> <button class="btn btn-danger" id="script-settings-submit">Save & Reload page</button>'+
             '</div>'+
         '</div>'+
     '</div>'+
 '</div>';
+
 $(settings_modal).insertBefore('.footer-v1');
+
+$('#plusreason-own-Prefixes').val(OwnReasons.prefixes);
+$('#plusreason-own-Reasons').val(OwnReasons.reasons);
+$('#plusreason-own-Postfixes').val(OwnReasons.postfixes);
+
 $('#script-settings-submit').on('click', function(event) {
+    // PlusReasons settings saving
+    var new_OwnReasons = {
+        prefixes: $('#plusreason-own-Prefixes').val().trim(),
+        reasons: $('#plusreason-own-Reasons').val().trim(),
+        postfixes: $('#plusreason-own-Postfixes').val().trim()
+    };
+
+    if (new_OwnReasons) {
+        console.log(new_OwnReasons);
+        storage.set('OwnReasons', new_OwnReasons);
+    }
+
+    // Key checking & saving
     if (($('#steamapi_id').val().length == 32) || ($('#steamapi_id').val() == "Kappa")) {
-        alert("Good choice!");
+        alert("Settings are saved!");
         storage.set('SteamApi', $('#steamapi_id').val());
         location.reload();
     } else {
-        alert("Wrong Key!");
+        alert("Wrong API Key! Please change it or Kappa.");
     }
 });
 $('#Kappa').on('click', function(event) {
@@ -178,7 +148,7 @@ if (steamapi === "Kappa") {
     $("#loading-spinner").remove();
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
-    })
+    });
 } else if (steamapi !== null && steamapi != "http://steamcommunity.com/dev/apikey") {
     $.ajax({
         url: "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=" + steamapi + "&format=json&steamids=" + steam_id,
@@ -207,7 +177,7 @@ if (steamapi === "Kappa") {
                     $("#loading-spinner").remove();
                     $(function () {
                         $('[data-toggle="tooltip"]').tooltip()
-                    })
+                    });
                 }
             });
         }
@@ -268,6 +238,52 @@ $(".comment > p").each(function(index, el) {
     $(this).wrap("<blockquote></blockquote>");
 });
 
+
+/**
+ * construct_buttons - PlusReasons Constructor
+ * @param  {object} OwnReasons   OwnReasons object
+ * @return {string} html         HTML snippet
+ */
+function construct_buttons(OwnReasons) {
+    var prefixes = OwnReasons.prefixes.split(',');
+    var reasons = OwnReasons.reasons.split(',');
+    var postfixes = OwnReasons.postfixes.split(',');
+
+    var html = '<br>';
+    html += each_type('Prefixes', prefixes);
+    html += each_type('Reasons', reasons);
+    html += each_type('Postfixes', postfixes);
+    html += '<button type="button" class="btn btn-link" id="reason_clear">Clear</button>';
+
+    return html;
+
+    function each_type(type, buttons) {
+        var place, color;
+        if (type == 'Prefixes') {
+            place = 'before';
+            color = 'warning';
+        } else if (type == 'Reasons'){
+            place = 'after';
+            color = 'default';
+        } else if (type == 'Postfixes'){
+            place = 'after';
+            color = 'danger';
+        }
+        var snippet = '<div class="btn-group"><a class="btn btn-' + color + ' dropdown-toggle" data-toggle="dropdown" href="#">' +
+            type + ' <span class="caret"></span></a><ul class="dropdown-menu">';
+
+        buttons.forEach(function(item, i, arr) {
+            snippet += '<li><a href="#" class="plusreason" data-place="' + place + '">' + item.trim() + '</a></li>';
+        });
+
+        snippet += '</ul></div>     ';
+        return snippet;
+    }
+}
+
+/**
+ * Queries Helper
+ */
 function GM_XHR() {
     this.type = null;
     this.url = null;
