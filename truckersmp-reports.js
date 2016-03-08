f2fc1288b9fc8f904070fb3d9b2f17be3a9f4d0a
CJMAXiK <i@cjmaxik.ru>
2016-03-08 17:16:52 +1000

    1.6.11 deployed!
    - Usability improvement (auto-focus on Reason input)

--- truckersmp-reports.js
+++ truckersmp-reports.js
@@ -2,7 +2,7 @@
 // @name         TruckersMP Reports Improved
 // @description  Only for TruckersMP Admins
 // @namespace    http://truckersmp.com/
-// @version      1.6.10
+// @version      1.6.11
 // @author       CJMAXiK
 // @icon         http://truckersmp.com/assets/images/favicon.png
 // @match        *://truckersmp.com/*/reports/view/*
@@ -25,7 +25,7 @@
 // ==/OpenUserJS==
 /* jshint -W097 */
 'use strict';
-var version = "1.6.10";
+var version = "1.6.11";
 console.log("TruckersMP Reports Improved INBOUND! Question - to @cjmaxik on Slack!");
 $('body > div.wrapper > div.breadcrumbs > div > h1').append(' Improved <span class="badge" data-toggle="tooltip" title="by @cjmaxik">' + version + '</span> <a href="#" data-toggle="modal" data-target="#script-settings"><i class="fa fa-cog" data-toggle="tooltip" title="Script settings"></i></a> <a href="http://bit.ly/BlameAnybody" target="_blank" id="version_detected" data-toggle="popover" data-trigger="focus" title="YAY! v.' + version + ' has been deployed!" data-content="Your handy-dandy script just updated! See what you get?"><i class="fa fa-question" data-toggle="tooltip" title="Changelog"></i></a> <i class="fa fa-spinner fa-spin" id="loading-spinner"></i>');
 
@@ -252,6 +252,8 @@ $('.plusreason').on('click', function(event) {
     } else {
         $('input[name="reason"]').val(reason_val + ' ' + $(this).html() + ' ');
     }
+    $('input[name="reason"]').focus();
+
 });
 
 // ===== Decline FTW =====
@@ -263,6 +265,7 @@ $('.plusdecline').on('click', function(event) {
     } else {
         $('#confirm-decline > div > div > form > div.modal-body > div > textarea').val(reason_val + ' ' + $(this).html());
     }
+    $('#confirm-decline > div > div > form > div.modal-body > div > textarea').focus();
 });
 
 $('button#reason_clear').on('click', function(event) {
