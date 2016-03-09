bf37379512e57ff22a864eb4f272bc1a9a7070b8
CJMAXiK <i@cjmaxik.ru>
2016-03-09 11:38:34 +1000

    1.6.12 deployed!
    - For now Aliases doesn't be empty if there is no aliases in Steam
    account.

--- truckersmp-reports.js
+++ truckersmp-reports.js
@@ -2,7 +2,7 @@
 // @name         TruckersMP Reports Improved
 // @description  Only for TruckersMP Admins
 // @namespace    http://truckersmp.com/
-// @version      1.6.11
+// @version      1.6.12
 // @author       CJMAXiK
 // @icon         http://truckersmp.com/assets/images/favicon.png
 // @match        *://truckersmp.com/*/reports/view/*
@@ -25,10 +25,10 @@
 // ==/OpenUserJS==
 /* jshint -W097 */
 'use strict';
-var version = "1.6.11";
+var version = "1.6.12";
 console.log("TruckersMP Reports Improved INBOUND! Question - to @cjmaxik on Slack!");
 $('body > div.wrapper > div.breadcrumbs > div > h1').append(' Improved <span class="badge" data-toggle="tooltip" title="by @cjmaxik">' + version + '</span> <a href="#" data-toggle="modal" data-target="#script-settings"><i class="fa fa-cog" data-toggle="tooltip" title="Script settings"></i></a> <a href="http://bit.ly/BlameAnybody" target="_blank" id="version_detected" data-toggle="popover" data-trigger="focus" title="YAY! v.' + version + ' has been deployed!" data-content="Your handy-dandy script just updated! See what you get?"><i class="fa fa-question" data-toggle="tooltip" title="Changelog"></i></a> <i class="fa fa-spinner fa-spin" id="loading-spinner"></i>');
- 
+
 // ===== Bootstrapping =====
 var now = moment();
 
@@ -190,12 +190,13 @@ if (steamapi === "Kappa") {
                 success: function(val){
                     var steam_aliases = val;
                     var aliases = "";
-                    for(var key in steam_aliases) {
-                        aliases += '<kbd  data-toggle="tooltip" title="' + steam_aliases[key].timechanged + '">' + steam_aliases[key].newname + '</kbd>   ';
-                    }
-                    aliases = '<tr><td>Aliases</td><td>'+ aliases +'</td></tr>';
-                    $(aliases).insertAfter('body > div.wrapper > div.container.content > div > div.clearfix > div:nth-child(1) > table > tbody > tr:nth-child(2)');
-
+                    if (!$.isEmptyObject(steam_aliases)) {
+                        for(var key in steam_aliases) {
+                            aliases += '<kbd  data-toggle="tooltip" title="' + steam_aliases[key].timechanged + '">' + steam_aliases[key].newname + '</kbd>   ';
+                        }
+                        aliases = '<tr><td>Aliases</td><td>'+ aliases +'</td></tr>';
+                        $(aliases).insertAfter('body > div.wrapper > div.container.content > div > div.clearfix > div:nth-child(1) > table > tbody > tr:nth-child(2)');
+                    };
                     // AFTER ALL!!!!!!!
                     $("#loading-spinner").hide();
                     $(function () {
