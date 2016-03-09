4d3ca840bd6a34c2c5399c812103f4ac1499ec06
CJMAXiK <i@cjmaxik.ru>
2016-03-09 12:51:01 +1000

    1.7.1 deployed!
    - Fix issue with Decline modal submit.

--- truckersmp-reports.js
+++ truckersmp-reports.js
@@ -2,7 +2,7 @@
 // @name         TruckersMP Reports Improved
 // @description  Only for TruckersMP Admins
 // @namespace    http://truckersmp.com/
-// @version      1.7.0
+// @version      1.7.1
 // @author       CJMAXiK
 // @icon         http://truckersmp.com/assets/images/favicon.png
 // @match        *://truckersmp.com/*/reports/view/*
@@ -25,7 +25,7 @@
 // ==/OpenUserJS==
 /* jshint -W097 */
 
-var version = "1.7.0";
+var version = "1.7.1";
 console.log("TruckersMP Reports Improved INBOUND! Question - to @cjmaxik on Slack!");
 $('body > div.wrapper > div.breadcrumbs > div > h1').append(' Improved <span class="badge" data-toggle="tooltip" title="by @cjmaxik">' + version + '</span> <a href="#" data-toggle="modal" data-target="#script-settings"><i class="fa fa-cog" data-toggle="tooltip" title="Script settings"></i></a> <a href="http://bit.ly/BlameAnybody" target="_blank" id="version_detected" data-toggle="popover" data-trigger="focus" title="YAY! v.' + version + ' has been deployed!" data-content="Your handy-dandy script just updated! See what you get?"><i class="fa fa-question" data-toggle="tooltip" title="Changelog"></i></a> <i class="fa fa-spinner fa-spin" id="loading-spinner"></i>');
 
@@ -354,7 +354,6 @@ $('#confirm-accept > div > div > form').on('submit', function(event) {
 });
 
 $('#confirm-decline > div > div > form').on('submit', function(event) {
-    event.preventDefault();
     var comment_check = $('#confirm-decline > div > div > form > div.modal-body > div > textarea').val();
     var error_style = {
         'border-color': '#a94442',
@@ -369,6 +368,7 @@ $('#confirm-decline > div > div > form').on('submit', function(event) {
 
     if (!comment_check) {
         $('#confirm-decline > div > div > form > div.modal-body > div > textarea').css(error_style);
+        event.preventDefault();
     } else {
         $('#confirm-decline > div > div > form > div.modal-body > div > textarea').css(normal_style);
     }
