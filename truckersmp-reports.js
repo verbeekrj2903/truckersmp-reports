baa9df9747ebe71f27101ec7d8b0bef6ed467fa2
Max Mekenya <i@cjmaxik.ru>
2016-03-27 21:46:42 +1000

    1.7.5 deployed!
    - Link parser fix

--- truckersmp-reports.js
+++ truckersmp-reports.js
@@ -2,7 +2,7 @@
 // @name         TruckersMP Reports Improved
 // @description  Only for TruckersMP Admins
 // @namespace    http://truckersmp.com/
-// @version      1.7.4
+// @version      1.7.5
 // @author       CJMAXiK
 // @icon         http://truckersmp.com/assets/images/favicon.png
 // @match        *://truckersmp.com/*/reports/view/*
@@ -25,7 +25,7 @@
 // ==/OpenUserJS==
 /* jshint -W097 */
 
-var version = "1.7.4";
+var version = "1.7.5";
 console.log("TruckersMP Reports Improved INBOUND! Question - to @cjmaxik on Slack!");
 $('body > div.wrapper > div.breadcrumbs > div > h1').append(' Improved <span class="badge" data-toggle="tooltip" title="by @cjmaxik">' + version + '</span> <a href="#" data-toggle="modal" data-target="#script-settings"><i class="fa fa-cog" data-toggle="tooltip" title="Script settings"></i></a> <a href="http://bit.ly/BlameAnybody" target="_blank" id="version_detected" data-toggle="popover" data-trigger="focus" title="YAY! v.' + version + ' has been deployed!" data-content="Your handy-dandy script just updated! See what you get?"><i class="fa fa-question" data-toggle="tooltip" title="Changelog"></i></a> <i class="fa fa-spinner fa-spin" id="loading-spinner"></i>');
 
@@ -47,7 +47,7 @@ $('input[id="perma.false"]').prop("checked", true);
 $('.content').each(function(){
     'use strict';
     var str = $(this).html();
-    var regex = /((http|https):\/\/([\w\-.]+)\/([^< )\s,.])+)/gi;
+    var regex = /((http|https):\/\/([\w\-.]+)\/([^< )\s,])+)/gi;
     var replaced_text = str.replace(regex, '<a href="$1" target="_blank" class="replaced">$1</a> <a href="#" class="jmdev_ca" data-link="$1"><i class="fa fa-link" data-toggle="tooltip" title="Click on me to get the shorter version to your clipboard"></i></a>');
     $(this).html(replaced_text);
 });
