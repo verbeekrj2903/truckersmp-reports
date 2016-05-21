73628c453bb8d4ce920393ba9dc2c24be1c389af
Max Mekenya <i@cjmaxik.ru>
2016-05-21 21:20:16 +1000

    2.1.5 deployed!
    - Trying to deal with abnormal work of Cloud

--- truckersmp-reports.js
+++ truckersmp-reports.js
@@ -2,7 +2,7 @@
 // @name         TruckersMP Reports Improved
 // @description  Only for TruckersMP Admins
 // @namespace    http://truckersmp.com/
-// @version      2.1.4
+// @version      2.1.5
 // @author       CJMAXiK
 // @icon         http://truckersmp.com/assets/images/favicon.png
 // @match        *://truckersmp.com/*/reports/view/*
@@ -31,7 +31,7 @@
 // ==/OpenUserJS==
 /* jshint -W097 */
 
-var version = "2.1.4";
+var version = "2.1.5";
 console.log("TruckersMP Reports Improved INBOUND! Question - to @cjmaxik on Slack!");
 $('body > div.wrapper > div.breadcrumbs > div > h1').append(' Improved <span class="badge" data-toggle="tooltip" title="by @cjmaxik">' + version + '</span> <a href="https://www.jmdev.ca/url/" target="_blank"><i class="fa fa-link" data-toggle="tooltip" title="URL Shortener"></i></a> <a href="#" data-toggle="modal" data-target="#script-settings"><i class="fa fa-cog" data-toggle="tooltip" title="Script settings"></i></a> <a href="http://bit.ly/BlameAnybody" target="_blank" id="version_detected" data-toggle="popover" data-trigger="focus" title="YAY! v.' + version + ' has been deployed!" data-content="Your handy-dandy script just updated! See what you get?"><i class="fa fa-question" data-toggle="tooltip" title="Changelog"></i></a>  <i class="fa fa-spinner fa-spin" id="loading-spinner"></i>');
 
@@ -44,8 +44,8 @@ var config = {
 };
 firebase.initializeApp(config);
 var database = firebase.database();
-var connectedRef = database.ref(".info/connected")
 
+var connectedRef = database.ref(".info/connected");
 function isConnected() {
     var connection;
    connectedRef.on("value", function(snap) {
@@ -57,6 +57,7 @@ function isConnected() {
     });
     return connection;
 };
+localStorage.removeItem('firebase:previous_websocket_failure');
 
 function writeUserData(steamapi, prefixes, reasons, postfixes, declines) {
     if(isConnected()) {
